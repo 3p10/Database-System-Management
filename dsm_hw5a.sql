fbdf2021b31ad952f3eae2859b2c0da2396fd538
3p10 <121188912+3p10@users.noreply.github.com>
2024-05-03 18:38:13 +0300

    Delete dsm_hw5a.sql

--- dsm_hw5a.sql
+++
@@ -1,8 +0,0 @@
--- Creating roles
-CREATE ROLE db_user;
-CREATE ROLE db_manager;
-CREATE ROLE db_owner;
-
-GRANT ALL PRIVILEGES ON Orders TO db_owner;
-GRANT SELECT ON Orders TO db_user;
-GRANT INSERT ON Orders TO db_manager;
